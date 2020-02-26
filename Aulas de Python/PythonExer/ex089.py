a728e91184ddc1b7c6ae276fdb6f74e8eec0269d
Hydeoshi <klauss.marques@gmail.com>
2020-02-26 15:27:43 -0300

    comentário

--- Aulas de Python/PythonExer/ex089.py
+++ Aulas de Python/PythonExer/ex089.py
@@ -2,3 +2,4 @@
  #* No final, mostre um boletim contendo média de cada um e permita que o usuário possa mostrar as notas de cada aluno individualmente.
  #! Os dados estão todos em uma única lista com 3 níveis de lista.
  # Faça com dicionários porque lista dentro de lista é ghetto programming
+ #! Vou pensar nisso.
